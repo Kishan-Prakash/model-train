0d54132f7c6489bfca5ba5a63362c366a6d3551c
Kishan-Prakash <kishanprakashpj@gmail.com>
2025-06-25 04:46:44 +0000

    2nd commit

--- train.py
+++ train.py
@@ -5,7 +5,7 @@ import joblib
 
 X, y = load_diabetes(return_X_y=True)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
 
 model = LinearRegression()
 
